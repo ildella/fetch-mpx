e1693ad7da9b99d46a00d77653c9cf76225fb57f
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2026-06-23 17:44:55 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import path from 'node:path'
+import {nostandard} from 'eslint-nostandard'
+import vitest from 'eslint-nostandard/vitest'
+// import svelte from 'eslint-nostandard/svelte'
+import {includeIgnoreFile} from '@eslint/compat'
+// import globals from 'globals'
+
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore')
+
+const ignores = [
+  'docs/*',
+]
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...nostandard.recommended,
+  // ...svelte,
+  vitest,
+  {
+    name: 'My new library',
+    ignores,
+    plugins: {},
+    languageOptions: {
+      // globals: {
+      //   ...globals.node,
+      //   ...globals.browse,
+      // },
+    },
+    rules: {},
+  },
+  {
+    name: 'ProductionCode',
+    files: ['src/**/*.js'],
+    rules: {},
+  },
+  {
+    name: 'Tests',
+    files: ['tests/**/*.js'],
+    rules: {},
+  },
+]
